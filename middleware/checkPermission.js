17f0c43027806945aa3b3b436e66c261b192a5d2
Biter01 <79320619+Biter01@users.noreply.github.com>
2021-11-26 23:39:55 +0100

    Add files via upload
    This is the project with a basic general chat and with a Login Register Page!!!

---
+++ middleware/checkPermission.js
@@ -0,0 +1,18 @@
+function checkPermission(req, res, next) {
+    const jwt = require('jsonwebtoken');
+    try{
+        //console.log(req.body);
+        const {token} = req.body;
+        //console.log(token);
+        //console.log(jwt);
+        const user = jwt.verify(token, 'sd*23jd3$h"w234!234l?ejk4rh');
+        var username = user.username;
+    } catch(err) {
+        //console.log(err);
+        return res.status(403).json({status: 'error', message: 'You are not permitted to see the resources'});
+    }
+    req.username = username;
+    next()
+}
+
+module.exports = checkPermission;
